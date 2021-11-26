995fdc5e3b4f27eeee9eb866d8f6ea7e46fc251e
TwitchViejoSarmi <95054605+TwitchViejoSarmi@users.noreply.github.com>
2021-11-25 20:16:56 -0500

    Comentarios
    Se insertaron comentarios en el codigo para un mayor entendimiento del mismo.

--- version-0.0/parcial-herramientas-0.0.py
+++ version-0.0/parcial-herramientas-0.0.py
@@ -1,37 +1,53 @@
 ##Parcial Herramientas
 """
-Código hecho por:
-Juan Esteban Becerra Gutiérrez
+Codigo hecho por:
+Juan Esteban Becerra Gutierrez
 ID: 8965694
 Alejandro Sarmiento Rivera
 ID: 8968284
 """
-precios = {"078": 12000, "236": 5400, "728": 7500}
+precios = {"078": 12000, "236": 5400, "728": 7500} ## Un diccionario con las id de los productos y sus respectivos precios.
+
+##Operacion para calcular el precio final del producto.
 def descuento(rol, prod, cant):
-  Valor = 0
+  Valor = 0 ##Valor inicial para poder usar la variable.
+
+  ##Si el valor se encuentra entre las claves del diccionario.
   if prod in precios:
-    v = precios[prod]
+    v = precios[prod] ##Se extrae el precio del producto.
+  
+  ##Si el rol del usuario es Estudiante.
   if rol == 1:
-    Valor = (v*cant) - ((v*cant)*0.5)
+    Valor = (v*cant) - ((v*cant)*0.5) ##Se aplica el descuento al precio del producto.
+  ##Si el rol del usuario es Profesor.
   elif rol == 2:
-    Valor = (v*cant) - ((v*cant)*0.2)
+    Valor = (v*cant) - ((v*cant)*0.2) ##Se aplica el descuento al precio del producto.
 
   return Valor
 
-
+##Operacion para pedir los valores al usuario.
 def leerimprimir():
   print("Ingrese cédula:")
   ced = int(input())
+
   print("\n¿Cuál es su rol?\n1: Estudiante\n2: Profesor")
   rol = int(input())
+  
+  ##Si el rol del usuario es Estudiante.
   if rol == 1:
     rolp = "estudiante"
+  ##Si el rol del usuario es Profesor.
   elif rol == 2:
     rolp = "profesor"
+  
   print("\nIngrese código de producto a comprar:")
   prod = str(input())
+  
   print("\nIngrese cantidad a comprar:")
   cant = int(input())
-  val = descuento(rol, prod, cant)
+  
+  val = descuento(rol, prod, cant) ##Se invoca la operacion anterior para hallar el precio final con los datos digitados por el usuario.
+  
   print("\nEl",rolp ,"con cedula", ced, "debe pagar", val, "por el producto",prod)
-leerimprimir()
+ 
+leerimprimir() ##Se invoca la ultima operacion para que funcione el programa.
